b37b79f5af7b37aa37c035902ca376a3814ac5b8
vvaith29 <johnkenee96@gmail.com>
2024-12-29 16:52:13 +0530

    Python Document file

---
+++ Edited Python Meterial .py
@@ -0,0 +1,21 @@
+import keyword
+
+print(keyword.kwlist) --> to get the list of keywords in python 
+--------------------------------------------------------------------------------
+Base conversion of int DT : 
+
+To convert any int DT to binary,octal,hexadecimal we can use the following:
+
+num = 45678
+print(bin(num)) # Returns the binary representtion of the integer 
+print(oct(num)) # Returns the octal representtion of the integer
+print(hex(num)) # Returns the hexadecimal representtion of the integer
+Complex DT :
+x= 10 + 20j
+print(x)
+print(type(x))  # Shows the object type 
+print(id(num))   # Shows the object address location or it's identity   
+-------------------------------------------------------------------------------
+Base conversion of int DT :
+
+
